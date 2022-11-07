319f15148850fa835816704084304004118c728c
agista <202210715295@mhs.ubharajaya.ac.id>
2022-11-07 14:38:57 +0700

    tugas2 sudah selesai
    kirim

---
+++ flyp2/no.2.py
@@ -0,0 +1,5 @@
+# pencabangan IF dengan 1 kasus
+nama =input("Masukkan Nama : ")
+if nama == "Agista Rizkiyanto" :
+    print("Hi", nama, "welcome")
+    print()
---
+++ flyp4/no.4.py
@@ -0,0 +1,18 @@
+# pengulangan pakai for
+for i in range(10):
+    print("pengulangan ke - ", i)
+
+print("\n")
+
+listMotor = ["Honda", "Yamaha", "Ducati", "Jupiter", "Suzuki"]
+for Motor in listMotor :
+    print(Motor)
+
+print("\n")
+
+# pengulangan pakai while
+angka = 1
+
+while angka > 10 :
+    angka += 1
+    print("contoh penggunaan sintax while")
